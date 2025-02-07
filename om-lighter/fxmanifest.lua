0226f869eaff7b24b0f07c0d01b1d0c5a211cbe2
Officer Mills <149747958+OfficerMills@users.noreply.github.com>
2025-02-07 01:35:09 -0600

    Update fxmanifest.lua
    Updated to use Tebex Asset Escrow.

--- om-lighter/fxmanifest.lua
+++ om-lighter/fxmanifest.lua
@@ -5,6 +5,18 @@ author 'OfficerMills'
 description 'A Zippo Lighter that lights fires.'
 version '1.0.0'
 
+-- This line is required when using asset escrow
+lua54 'yes'
+
+-- This line tells FiveM to enforce encryption on scripts
+escrow_enforce 'yes'
+
 shared_script 'config.lua'
 client_script 'client.lua'
 server_script 'server.lua'
+
+-- Anything you list here will be ignored and not encrypted
+escrow_ignore {
+    'config.lua',
+    'IMAGES/*'
+}
